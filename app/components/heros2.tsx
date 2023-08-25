cf187cf083ba6d1bdd619fed5b2951d77976d70e
Mesha Vegas <84249301+Meshavegas@users.noreply.github.com>
2023-08-25 02:29:47 +0100

    fix hero

--- app/components/heros2.tsx
+++ app/components/heros2.tsx
@@ -77,10 +77,10 @@ const Heros2 = () => {
             <p className=" w-5/6 md:w-1/3">
               <motion.h2
                 variants={{
-                  hidden: { opacity: 0, x: -75 },
+                  hidden: { opacity: 1, x: -75 },
                   visible: { opacity: 1, x: 0 },
                 }}
-                initial="hidden"
+                initial="visible"
                 animate={control}
                 transition={{ duration: 0.5, delay: 0.25 }}
                 className=" text-2xl font-extrabold text-white"
@@ -89,10 +89,10 @@ const Heros2 = () => {
               </motion.h2>
               <motion.p
                 variants={{
-                  hidden: { opacity: 0, x: -75 },
+                  hidden: { opacity: 1, x: -75 },
                   visible: { opacity: 1, x: 0 },
                 }}
-                initial="hidden"
+                initial="visible"
                 animate={control}
                 transition={{ duration: 0.5, delay: 0.25 }}
                 className=" text-xl"
@@ -105,226 +105,10 @@ const Heros2 = () => {
             </p>
             <motion.div
               variants={{
-                hidden: { opacity: 0, x: 75 },
+                hidden: { opacity: 1, x: 75 },
                 visible: { opacity: 1, x: 0 },
               }}
-              initial="hidden"
-              animate={control}
-              transition={{ duration: 0.5 }}
-              className=" w-5/6 transform transition duration-500 -rotate-6 hover:-rotate-0 back flex flex-col justify-center items-center gap-4 bg-light py-10 px-3 md:py-10 md:px-8  rounded-xl md:w-1/3 "
-            >
-              <Image
-                src="/hero.jpg"
-                alt="logo"
-                width={1080}
-                height={1080}
-                className=" lg:w-full rounded-[3rem] shadow-2xl shadow-black"
-              />
-            </motion.div>
-          </motion.div>
-        </SwiperSlide>
-        <SwiperSlide>
-          <motion.div
-            ref={ref}
-            className="bg-light lg:px-0 lg:py-60 px-5 py-28 w-full  gap-5 justify-center flex items-center md:flex-row flex-col"
-          >
-            <p className=" w-5/6 md:w-1/3">
-              <motion.h2
-                variants={{
-                  hidden: { opacity: 0, x: -75 },
-                  visible: { opacity: 1, x: 0 },
-                }}
-                initial="hidden"
-                animate={control}
-                transition={{ duration: 0.5, delay: 0.25 }}
-                className=" text-2xl font-extrabold text-white"
-              >
-                Nos performance agreable
-              </motion.h2>
-              <motion.p
-                variants={{
-                  hidden: { opacity: 0, x: -75 },
-                  visible: { opacity: 1, x: 0 },
-                }}
-                initial="hidden"
-                animate={control}
-                transition={{ duration: 0.5, delay: 0.25 }}
-                className=" text-xl"
-              >
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui
-                eos repellat quibusdam, natus et labore dignissimos quisquam
-                perspiciatis sequi dicta officia doloremque, vel tenetur.
-                Possimus quaerat porro reiciendis necessitatibus alias?
-              </motion.p>
-            </p>
-            <motion.div
-              variants={{
-                hidden: { opacity: 0, x: 75 },
-                visible: { opacity: 1, x: 0 },
-              }}
-              initial="hidden"
-              animate={control}
-              transition={{ duration: 0.5 }}
-              className=" w-5/6 transform transition duration-500 -rotate-6 hover:-rotate-0 back flex flex-col justify-center items-center gap-4 bg-light py-10 px-3 md:py-10 md:px-8  rounded-xl md:w-1/3 "
-            >
-              <Image
-                src="/hero.jpg"
-                alt="logo"
-                width={1080}
-                height={1080}
-                className=" lg:w-full rounded-[3rem] shadow-2xl shadow-black"
-              />
-            </motion.div>
-          </motion.div>
-        </SwiperSlide>
-        <SwiperSlide>
-          <motion.div
-            ref={ref}
-            className="bg-light lg:px-0 lg:py-60 px-5 py-28 w-full  gap-5 justify-center flex items-center md:flex-row flex-col"
-          >
-            <p className=" w-5/6 md:w-1/3">
-              <motion.h2
-                variants={{
-                  hidden: { opacity: 0, x: -75 },
-                  visible: { opacity: 1, x: 0 },
-                }}
-                initial="hidden"
-                animate={control}
-                transition={{ duration: 0.5, delay: 0.25 }}
-                className=" text-2xl font-extrabold text-white"
-              >
-                Nos performance agreable
-              </motion.h2>
-              <motion.p
-                variants={{
-                  hidden: { opacity: 0, x: -75 },
-                  visible: { opacity: 1, x: 0 },
-                }}
-                initial="hidden"
-                animate={control}
-                transition={{ duration: 0.5, delay: 0.25 }}
-                className=" text-xl"
-              >
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui
-                eos repellat quibusdam, natus et labore dignissimos quisquam
-                perspiciatis sequi dicta officia doloremque, vel tenetur.
-                Possimus quaerat porro reiciendis necessitatibus alias?
-              </motion.p>
-            </p>
-            <motion.div
-              variants={{
-                hidden: { opacity: 0, x: 75 },
-                visible: { opacity: 1, x: 0 },
-              }}
-              initial="hidden"
-              animate={control}
-              transition={{ duration: 0.5 }}
-              className=" w-5/6 transform transition duration-500 -rotate-6 hover:-rotate-0 back flex flex-col justify-center items-center gap-4 bg-light py-10 px-3 md:py-10 md:px-8  rounded-xl md:w-1/3 "
-            >
-              <Image
-                src="/hero.jpg"
-                alt="logo"
-                width={1080}
-                height={1080}
-                className=" lg:w-full rounded-[3rem] shadow-2xl shadow-black"
-              />
-            </motion.div>
-          </motion.div>
-        </SwiperSlide>
-        <SwiperSlide>
-          <motion.div
-            ref={ref}
-            className="bg-light lg:px-0 lg:py-60 px-5 py-28 w-full  gap-5 justify-center flex items-center md:flex-row flex-col"
-          >
-            <p className=" w-5/6 md:w-1/3">
-              <motion.h2
-                variants={{
-                  hidden: { opacity: 0, x: -75 },
-                  visible: { opacity: 1, x: 0 },
-                }}
-                initial="hidden"
-                animate={control}
-                transition={{ duration: 0.5, delay: 0.25 }}
-                className=" text-2xl font-extrabold text-white"
-              >
-                Nos performance agreable
-              </motion.h2>
-              <motion.p
-                variants={{
-                  hidden: { opacity: 0, x: -75 },
-                  visible: { opacity: 1, x: 0 },
-                }}
-                initial="hidden"
-                animate={control}
-                transition={{ duration: 0.5, delay: 0.25 }}
-                className=" text-xl"
-              >
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui
-                eos repellat quibusdam, natus et labore dignissimos quisquam
-                perspiciatis sequi dicta officia doloremque, vel tenetur.
-                Possimus quaerat porro reiciendis necessitatibus alias?
-              </motion.p>
-            </p>
-            <motion.div
-              variants={{
-                hidden: { opacity: 0, x: 75 },
-                visible: { opacity: 1, x: 0 },
-              }}
-              initial="hidden"
-              animate={control}
-              transition={{ duration: 0.5 }}
-              className=" w-5/6 transform transition duration-500 -rotate-6 hover:-rotate-0 back flex flex-col justify-center items-center gap-4 bg-light py-10 px-3 md:py-10 md:px-8  rounded-xl md:w-1/3 "
-            >
-              <Image
-                src="/hero.jpg"
-                alt="logo"
-                width={1080}
-                height={1080}
-                className=" lg:w-full rounded-[3rem] shadow-2xl shadow-black"
-              />
-            </motion.div>
-          </motion.div>
-        </SwiperSlide>
-        <SwiperSlide>
-          <motion.div
-            ref={ref}
-            className="bg-light lg:px-0 lg:py-60 px-5 py-28 w-full  gap-5 justify-center flex items-center md:flex-row flex-col"
-          >
-            <p className=" w-5/6 md:w-1/3">
-              <motion.h2
-                variants={{
-                  hidden: { opacity: 0, x: -75 },
-                  visible: { opacity: 1, x: 0 },
-                }}
-                initial="hidden"
-                animate={control}
-                transition={{ duration: 0.5, delay: 0.25 }}
-                className=" text-2xl font-extrabold text-white"
-              >
-                Nos performance agreable
-              </motion.h2>
-              <motion.p
-                variants={{
-                  hidden: { opacity: 0, x: -75 },
-                  visible: { opacity: 1, x: 0 },
-                }}
-                initial="hidden"
-                animate={control}
-                transition={{ duration: 0.5, delay: 0.25 }}
-                className=" text-xl"
-              >
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui
-                eos repellat quibusdam, natus et labore dignissimos quisquam
-                perspiciatis sequi dicta officia doloremque, vel tenetur.
-                Possimus quaerat porro reiciendis necessitatibus alias?
-              </motion.p>
-            </p>
-            <motion.div
-              variants={{
-                hidden: { opacity: 0, x: 75 },
-                visible: { opacity: 1, x: 0 },
-              }}
-              initial="hidden"
+              initial="visible"
               animate={control}
               transition={{ duration: 0.5 }}
               className=" w-5/6 transform transition duration-500 -rotate-6 hover:-rotate-0 back flex flex-col justify-center items-center gap-4 bg-light py-10 px-3 md:py-10 md:px-8  rounded-xl md:w-1/3 "
