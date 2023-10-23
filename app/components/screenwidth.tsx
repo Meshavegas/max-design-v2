e7b320b923c055cba426b1b3b5f3f8dbbdecf8b7
Mesha Vegas <84249301+Meshavegas@users.noreply.github.com>
2023-10-23 06:39:00 +0100

    Update screenwidth.tsx

--- app/components/screenwidth.tsx
+++ app/components/screenwidth.tsx
@@ -1,10 +1,14 @@
 import React from "react";
 
-const Screenwidth = ({ children, background, padding }: any) => {
+const Screenwidth = ({ children, background, padding, id }: any) => {
   // console.log(padding);
 
   const paddin = padding ? padding : " md:px-40 md:py-12 px-5 py-10";
-  return <div className={` ${paddin} ${background} `}>{children}</div>;
+  return (
+    <div className={` ${paddin} ${background} `} id={id}>
+      {children}
+    </div>
+  );
 };
 
 export default Screenwidth;
