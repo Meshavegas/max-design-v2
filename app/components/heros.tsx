758159bd76bb383d5165e6065cab969aa2ea1a6c
Mesha Vegas <84249301+Meshavegas@users.noreply.github.com>
2023-10-23 06:38:40 +0100

    Update heros.tsx

--- app/components/heros.tsx
+++ app/components/heros.tsx
@@ -4,12 +4,12 @@ import { FaWhatsapp } from "react-icons/fa";
 
 const Heros = () => {
   return (
-    <div className=" back flex flex-col justify-center items-center gap-4 bg-light py-10 px-3 md:py-10 bg-opacity-60 md:px-8  rounded-2xl w-fit ">
-      <h1 className=" text-3xl md:text-5xl text-white text-center font-extrabold">
+    <div className=" back flex flex-col justify-center items-center gap-4 bg-light py-10 px-3 md:py-10 bg-opacity-60 md:px-8  rounded-2xl w-fit">
+      <h1 className=" text-3xl md:text-5xl text-white text-center font-extrabold font-b93 tracking-[0.1em] uppercase">
         We are a creative agency : le design vous rend visible
       </h1>
       <div className="flex flex-col items-center gap-4 md:w-full">
-        <h1 className=" text-2xl underline underline-offset-4 text-[#e459ac] gap-4 md:text-4xl">
+        <h1 className=" text-2xl underline underline-offset-4 text-[#e459ac] gap-4 md:text-4xl font-kazy">
           Nous Contacter
         </h1>
         <div className=" flex gap-4 text-2xl flex-col mt-2 lg:flex-row lg:w-1/2">
